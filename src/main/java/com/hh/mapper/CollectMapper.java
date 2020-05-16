2c1ead25e1cd872bd9d85d3e965678bd93f8902f
微凉 <36558727+Xhofe@users.noreply.github.com>
2020-05-16 20:12:41 +0800

    init mapper --user=Xh0fe --story=1003064

---
+++ src/main/java/com/hh/mapper/CollectMapper.java
@@ -0,0 +1,9 @@
+package com.hh.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface CollectMapper {
+}
