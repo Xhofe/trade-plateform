6bfd90476d2f62af7c9d5b9e22b6c774616cd88d
微凉 <36558727+Xhofe@users.noreply.github.com>
2020-05-18 17:11:37 +0800

    --user=Xh0fe --story=1003080

--- src/main/java/com/hh/Controller/UserController.java
+++ src/main/java/com/hh/Controller/UserController.java
@@ -8,8 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/user")
 public class UserController {
     private UserService userService;
@@ -21,6 +22,7 @@ public class UserController {
 
     @PostMapping("/addUser")
     public Object addUser(@RequestBody User user){
+        System.out.println(user);
         return userService.addUser(user);
     }
 }
