4a4b14bf621cbbd4f865c33bb4bc945676f480aa
微凉 <36558727+Xhofe@users.noreply.github.com>
2020-06-12 13:52:48 +0800

    --user=Xh0fe change pathutil to pass test

--- src/main/java/com/hh/util/PathUtil.java
+++ src/main/java/com/hh/util/PathUtil.java
@@ -6,10 +6,10 @@ import java.io.File;
 
 public class PathUtil {
     static {
-        ApplicationHome h = new ApplicationHome(PathUtil.class);
-        File jarF = h.getSource();
-        ROOT_PATH=jarF.getParentFile().toString();
-//        ROOT_PATH="/home/Xhofe/personal-mall";
+//        ApplicationHome h = new ApplicationHome(PathUtil.class);
+//        File jarF = h.getSource();
+//        ROOT_PATH=jarF.getParentFile().toString();
+        ROOT_PATH="/home/Xhofe/personal-mall";
     }
     private static String ROOT_PATH;
     public static String getRootPath(){
